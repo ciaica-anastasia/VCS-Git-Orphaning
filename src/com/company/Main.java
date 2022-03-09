87b42383ee4b111e2aec5eccdf518df5be7ef156
Anastasia <chaika.a@phystech.edu>
2022-03-09 11:47:07 +0300

    Third commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Second commit into master");
+        System.out.println("Third commit into master");
     }
 }
